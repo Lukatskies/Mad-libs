d289662f6d1fbc47c983c34f61b030e909be53c2
LUKE SHEESLEY <Luke.sheesley@uvm.edu>
2024-10-21 11:50:47 -0400

    message

--- index.js
+++ index.js
@@ -1,37 +1,26 @@
-//Function for inputs//
-//type of bird//
+
+// Function to get input value
+function getInputValue(id) {
+    const inputElement = document.getElementById(id);
+    return inputElement.value;
+}
+
+// Function to generate the madlib
 function generateMadlib() {
-    const typeOfBirdInput = document.getElementById('typeOfBird');
-    const typeOfBird = typeOfBirdInput.value; 
-    
+    const typeOfBird = getInputValue('typeOfBird');
+    const roommateName = getInputValue('roommateName');
+    const noun = getInputValue('noun');
+    const liquid = getInputValue('liquid');
+    const bodyPart = getInputValue('bodyPart');
 
-//roomate name//
-    const roommateNameInput = document.getElementById('roommateName');
-    const roommateName = roommateNameInput.value;
-
-
-//noun//
-    const nounInput = document.getElementById('noun');
-    const noun = nounInput.value;
-    
-
-//liquid//
-    const liquidInput = document.getElementById('liquid');
-    const liquid = liquidInput.value;
-    
-
-//bodypart//
-    const bodyPartInput = document.getElementById('bodyPart');
-    const bodyPart = bodyPartInput.value;
-
-
-// construct text
-        function madLibText() {
-            return `It was a cold October day. I woke to the smell of ${typeOfBird} roasting in the kitchen. 
-            My roommate said, 'See if ${roommateName} needs a fresh ${noun}.'  So I carried a glass of ${liquid} 
+    // Construct text
+    function madLibText() {
+        return `It was a cold October day. I woke to the smell of ${typeOfBird} roasting in the kitchen. 
+            My roommate said, 'See if ${roommateName} needs a fresh ${noun}.' So I carried a glass of ${liquid} 
             into ${roommateName}'s room. When I got there, I couldn't believe my ${bodyPart}!`;
-        }
-// Call mad lib text function
-        document.getElementById('mad-libs').innerText = madLibText();
+    }
+
+    // Call mad lib text function
+    document.getElementById('mad-libs').innerText = madLibText();
 }
     
